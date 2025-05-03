e9de42b0d1bbbf5c21b6aa3b90880ddb2b9ad3c9
aliyun1153623907 <3153566913@qq.com>
2025-05-03 17:17:00 +0800

    feat: 集成spring

---
+++ xiaou-api/src/main/java/com/xiaou/web/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.xiaou.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "欢迎来到AI-Interview";
+    }
+}
